200e8bfd919549676a084aab657cd861a43da2c6
Spiros <spiroszoupas@hotmail.com>
2023-11-15 23:27:53 +0000

    index.js now renders requestCertificateForm component

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { RequestCertificateForm } from './pages/requestCertificate';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RequestCertificateForm></RequestCertificateForm>
   </React.StrictMode>
 );
 
