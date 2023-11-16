e7ac231b986057cc416f84bce47ecac3969173ae
Spiros <spiroszoupas@hotmail.com>
2023-11-16 10:29:19 +0000

    set up routes for page navigation in App.js, index.js now renders App.js

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { RequestCertificateForm } from './pages/requestCertificate';
+import { RequestList } from './pages/certificateList';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RequestCertificateForm></RequestCertificateForm>
+    <App></App>
   </React.StrictMode>
 );
 
